3f8c91c0f81ae865a83d4abaf4dac61b965113f4
munna3438 <mdmashudurrahmanmunna@gmail.com>
2023-10-01 07:23:09 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const leapYear = document.getElementById("leapYear");
+const submit = document.getElementById("submit");
+
+const isLeapYear = (year) => {
+  if (year % 4 !== 0) {
+    return "sorry, your birthday not a leap year";
+  } else if (year % 100 !== 0) {
+    return "congratulations, your birthday is a leap year";
+  } else if (year % 400 !== 0) {
+    return "sorry, your birthday not a leap year";
+  } else {
+    return "congratulations, your birthday is a leap year";
+  }
+};
+
+submit.addEventListener("click", function () {
+  const year = leapYear.value;
+  const result = isLeapYear(year);
+  alert(result);
+});
