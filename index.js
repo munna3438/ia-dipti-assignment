12d727fa281619279c17ae80923b3d78f700872b
munna3438 <mdmashudurrahmanmunna@gmail.com>
2023-10-01 10:57:03 +0600

    validate number and empty string

--- index.js
+++ index.js
@@ -2,17 +2,17 @@ const leapYear = document.getElementById("leapYear");
 const submit = document.getElementById("submit");
 
 const isLeapYear = (year) => {
-  if (!Number.isNaN(year)) {
-    return "please enter a valid year";
-  } else if (year % 4 !== 0) {
-    return "sorry, your birthday not a leap year";
-  } else if (year % 100 !== 0) {
-    return "congratulations, your birthday is a leap year";
-  } else if (year % 400 !== 0) {
-    return "sorry, your birthday not a leap year";
-  } else {
-    return "congratulations, your birthday is a leap year";
-  }
+  if (isNaN(year)) {
+    return "Invalid number"; // Check if the input is not a number
+  } else if (year == "") {
+    return "Please enter a number";
+  } else if (year % 4 === 0) {
+    if (year % 100 === 0) {
+      if (year % 400 === 0)
+        return "Congratulations, your birthday is a leap year";
+      else return "Sorry, your birthday is not a leap year";
+    } else return "Congratulations, your birthday is a leap year";
+  } else return "Sorry, your birthday is not a leap year";
 };
 
 submit.addEventListener("click", function () {
